7c799e804fa9b00c93e92c47603d433ac7e24f29
raselsm530-ai <raselsm530@gmail.com>
2025-12-14 08:54:21 +0600

    Update history.js

--- history.js
+++ history.js
@@ -1,28 +1,9 @@
-/* লগইন চেক */
-if (localStorage.getItem("loggedIn") !== "true") {
-    window.location.href = "login.html";
-}
+let phone = localStorage.getItem("currentUser");
+let user = JSON.parse(localStorage.getItem(phone));
 
-/* ইউজার লোড */
-let currentPhone = localStorage.getItem("currentUser");
-let userData = JSON.parse(localStorage.getItem(currentPhone));
-
-let historyList = document.getElementById("historyList");
-
-if (!userData || !userData.transactions || userData.transactions.length === 0) {
-    historyList.innerHTML = "<p>কোনো উত্তোলন হিস্টরি নেই</p>";
-} else {
-    userData.transactions
-        .filter(t => t.type === "Withdraw")
-        .reverse()
-        .forEach(t => {
-            let div = document.createElement("div");
-            div.className = "menu-card";
-            div.innerHTML = `
-                <h4>উত্তোলন</h4>
-                <p>পরিমাণ: ${t.amount} ৳</p>
-                <p>${t.date}</p>
-            `;
-            historyList.appendChild(div);
-        });
-}
+user.transactions.forEach(t => {
+    let div = document.createElement("div");
+    div.className = "menu-card";
+    div.innerHTML = `<b>${t.amount} ৳</b><br>${t.time}`;
+    list.appendChild(div);
+});
