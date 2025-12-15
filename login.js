a6ad7fb82736b035e5bbb30a8119c0edb6f59ab3
raselsm530-ai <raselsm530@gmail.com>
2025-12-15 07:55:13 +0600

    Update login.js

--- login.js
+++ login.js
@@ -21,5 +21,5 @@ function loginUser() {
     localStorage.setItem("currentUser", phone);
 
     alert("লগইন সফল 🎉");
-    window.location.href = "home.html";
+    window.location.href = "packages.html";
 }
