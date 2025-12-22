877906ffed574cee42ea009d3c21f5acca3cb1fa
raselsm530-ai <raselsm530@gmail.com>
2025-12-22 13:39:55 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,18 @@
-// Import Firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCH_qTmU7QIUC4RKoYEX7QaXpPUNSN8mBI",
-  authDomain: "smart-earning-platform-bd.firebaseapp.com",
-  databaseURL: "https://smart-earning-platform-bd-default-rtdb.firebaseio.com",
-  projectId: "smart-earning-platform-bd",
-  storageBucket: "smart-earning-platform-bd.firebasestorage.app",
-  messagingSenderId: "1076796437288",
-  appId: "1:1076796437288:web:b6a6497cf4795dc3c0561d",
-  measurementId: "G-ZQ5BT4LT2D"
+    apiKey: "AIzaSyCH_qTmU7QIUC4RKoYEX7QaXpPUNSN8mBI",
+    authDomain: "smart-earning-platform-bd.firebaseapp.com",
+    databaseURL: "https://smart-earning-platform-bd-default-rtdb.firebaseio.com",
+    projectId: "smart-earning-platform-bd",
+    storageBucket: "smart-earning-platform-bd.firebasestorage.app",
+    messagingSenderId: "1076796437288",
+    appId: "1:1076796437288:web:b6a6497cf4795dc3c0561d",
+    measurementId: "G-ZQ5BT4LT2D"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
